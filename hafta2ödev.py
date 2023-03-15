71bc7cc0e3b96c85f6a22ddebb5e4833298d2e84
humanur <127956689+humanur@users.noreply.github.com>
2023-03-15 19:48:14 +0300

    Update and rename hafta2ödev to hafta2ödev.py

---
+++ hafta2ödev.py
@@ -0,0 +1,7 @@
+#Aldığı isim soy isim ile listeye öğrenci ekleyen
+Aldığı isim soy isim ile eşleşen değeri listeden kaldıran
+Listeye birden fazla öğrenci eklemeyi mümkün kılan
+Listeden birden fazla öğrenci silmeyi mümkün kılan
+Listedeki tüm öğrencileri tek tek ekrana yazdıran
+Öğrencinin listedeki index numarası öğrenci numarası olarak kabul edildiğini düşünerek öğrencinin numarasını öğrenmeyi mümkün kılan
+
